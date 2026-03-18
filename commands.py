42d50dd4e61071c0bcfa45acc58d239c97d0b1f8
Tobias Møller Larsen <Tobias-alleroed@hotmail.com>
2026-03-18 08:38:25 +0100

    precedence and removed old version

--- commands.py
+++ commands.py
@@ -14,71 +14,72 @@ tank_drive.gyro = GyroSensor()
 
 
 def forward(speed, rotations, pos, seconds, brake, block):
-    if rotations:
+    if seconds:
+        tank_drive.on_for_seconds(left_speed=speed, right_speed=speed, seconds=seconds, brake=brake, block=block)
+    elif rotations:
         tank_drive.on_for_rotations(left_speed=speed, right_speed=speed, rotations=rotations, brake=brake, block=block)
     elif pos:
-        tank_drive.on_to_position(left_speed=speed, right_speed=speed, pos=pos, brake=brake, block=block)
-    elif seconds:
-        tank_drive.on_for_seconds(left_speed=speed, right_speed=speed, seconds=seconds, brake=brake, block=block)
+        tank_drive.on_for_position(left_speed=speed, right_speed=speed, pos=pos, brake=brake, block=block)
     else:
         return
         
 
 def backward(speed, rotations, pos, seconds, brake, block):
-    if rotations:
-        tank_drive.on_for_rotations(left_speed=-speed, right_speed=-speed, rotations=rotations, brake=brake, block=block)
+    if seconds:
+        tank_drive.on_for_seconds(left_speed=-speed, right_speed=speed, seconds=seconds, brake=brake, block=block)
+    elif rotations:
+        tank_drive.on_for_rotations(left_speed=-speed, right_speed=speed, rotations=rotations, brake=brake, block=block)
     elif pos:
-        tank_drive.on_to_position(left_speed=-speed, right_speed=-speed, pos=pos, brake=brake, block=block)
-    elif seconds:
-        tank_drive.on_for_seconds(left_speed=-speed, right_speed=-speed, seconds=seconds, brake=brake, block=block)
+        tank_drive.on_for_position(left_speed=-speed, right_speed=speed, pos=pos, brake=brake, block=block)
     else:
         return
     
 # TODO: Should change to target_angle when protocol is updated
-def turn_left(speed, rspeed, lspeed, rotations, pos, seconds, degrees, brake, block):
+def turn_left(speed, rspeed, lspeed, rotations, pos, seconds, turn_angle, brake, block):
     # Calibrate the gyro to eliminate drift, and to initialize the current angle as 0
     tank_drive.gyro.calibrate()
-    if degrees:
-        tank_drive.turn_left(speed=speed, degrees=degrees, brake=brake)
-    elif  rotations:
+    if turn_angle:
+        tank_drive.turn_left(speed=speed, turn_angle=turn_angle, brake=brake)
+    elif  seconds:
+        tank_drive.on_for_seconds(lspeed, rspeed, seconds, brake, block)
+    elif rotations:
         tank_drive.on_for_rotations(lspeed, rspeed, rotations, brake, block)
     elif pos:
-        tank_drive.on_to_position(lspeed, rspeed, pos, brake, block)
-    elif seconds:
-        tank_drive.on_for_seconds(lspeed, rspeed, seconds, brake, block)
+        tank_drive.on_for_position(lspeed, rspeed, pos, brake, block)
     else:
         return
     
 
-def turn_right(speed, rspeed, lspeed, rotations, pos, seconds, degrees, brake, block):
+def turn_right(speed, rspeed, lspeed, rotations, pos, seconds, turn_angle, brake, block):
     # Calibrate the gyro to eliminate drift, and to initialize the current angle as 0
     tank_drive.gyro.calibrate()
-    if degrees:
-        tank_drive.turn_right(speed, -degrees, brake)
-    elif  rotations:
+    if turn_angle:
+        tank_drive.turn_left(speed=speed, turn_angle=turn_angle, brake=brake)
+    elif  seconds:
+        tank_drive.on_for_seconds(lspeed, rspeed, seconds, brake, block)
+    elif rotations:
         tank_drive.on_for_rotations(lspeed, rspeed, rotations, brake, block)
     elif pos:
-        tank_drive.on_to_position(lspeed, rspeed, pos, brake, block)
-    elif seconds:
-        tank_drive.on_for_seconds(lspeed, rspeed, seconds, brake, block)
+        tank_drive.on_for_position(lspeed, rspeed, pos, brake, block)
     else:
         return
+    
 # Speed is negative because of the gearing on the current robot
 def balls_in(speed, rotations, seconds, brake, block):
-    if rotations:
-        ballMotor.on_for_rotations(-speed, rotations, brake, block)
-    elif seconds:
+    if seconds:
         ballMotor.on_for_seconds(-speed, seconds, brake, block)
+    elif rotations:
+        ballMotor.on_for_rotations(-speed, rotations, brake, block)
     else:
         # Default in
         ballMotor.on(-speed, brake, block)
     
 
 def balls_out(speed, rotations, seconds, brake, block):
-    if rotations:
-        ballMotor.on_for_rotations(speed, rotations, brake, block)
-    elif seconds:
+    if seconds:
         ballMotor.on_for_seconds(speed, seconds, brake, block)
+    elif rotations:
+        ballMotor.on_for_rotations(speed, rotations, brake, block)
     else:
         # Default out
         ballMotor.on(speed, brake, block)
--- robot.py
+++ robot.py
@@ -76,7 +76,7 @@ def main():
                         conn.sendall(reply)
                         # Arguments in order:
                         # 1. inst_id, 2. rspeed, 3. lspeed, 4. speed, 5. rotations, 
-                        # 6. pos, 7. seconds, 8. degrees (for turn_degrees(...) ), 9. brake, 10. block, 11. talk
+                        # 6. pos, 7. seconds, 8. turn_angle (for turn_degrees(...) ), 9. brake, 10. block, 11. talk
                     else:
                         if args[0]:
                             inst_id = args[0]
@@ -107,9 +107,9 @@ def main():
                         else:
                             seconds = None
                         if args[7]:
-                            degrees = float(args[7])
+                            turn_angle = float(args[7])
                         else: 
-                            degrees = None
+                            turn_angle = None
                         if args[8]:
                             brake = bool(args[8])
                         else:
@@ -129,9 +129,9 @@ def main():
                         elif command == "c_bwd" and speed and (rotations or pos or seconds):
                             backward(speed, rotations, pos, seconds, brake, block)
                         elif command == "c_tl":
-                            turn_left(speed, lspeed, rspeed, rotations, pos, seconds, degrees, brake, block)
+                            turn_left(speed, lspeed, rspeed, rotations, pos, seconds, turn_angle, brake, block)
                         elif command == "c_tr":
-                            turn_right(speed, lspeed, rspeed, rotations, pos, seconds, degrees, brake, block)
+                            turn_right(speed, lspeed, rspeed, rotations, pos, seconds, turn_angle, brake, block)
                         elif command == "c_bin":
                             balls_in(speed, rotations, seconds, brake, block)
                         elif command == "c_bout":
@@ -151,73 +151,6 @@ def main():
                             conn.sendall(reply)
                     reply = ("ACK: " + str(instructions) + "\n").encode("utf-8")
                     conn.sendall(reply)
-                """
-                if len(arguments) == 2:
-                    if arguments[0] == "left":
-                        global monitoring
-                        monitoring = True
-                        monitor_thread = threading.Thread(target=monitor_gyro, args=(tank_drive.gyro), daemon=True)
-                        monitor_thread.start()
-                        # Pivot left
-                        tank_drive.turn_degrees(
-                            speed=SpeedPercent(100),
-                            target_angle=int(arguments[1])
-                        )
-                        monitoring = False
-                        monitor_thread.join(timeout=1)
-                    elif arguments[0] == "right":
-                        monitoring = True
-                        monitor_thread = threading.Thread(target=monitor_gyro, args=(tank_drive.gyro,), daemon=True)
-                        monitor_thread.start()
-                        # Pivot right
-                        tank_drive.turn_degrees(
-                            speed=SpeedPercent(30),
-                            target_angle=-int(arguments[1])
-                        )
-                        monitoring = False
-                        monitor_thread.join(timeout=1)
-                else:
-                    if arguments[0] == "l":
-                        tank_drive.on_for_rotations(SpeedPercent(50), SpeedPercent(-50), 1)
-
-                    elif arguments[0] == "r":
-                        tank_drive.on_for_rotations(SpeedPercent(-50), SpeedPercent(50), 1)
-
-                    elif arguments[0] == "f":
-                        tank_drive.on_for_rotations(SpeedPercent(50), SpeedPercent(50), 1, False, False)
-
-                    elif arguments[0] == "on":
-                        ballMotor.on(SpeedPercent(-100))
-                    elif arguments[0] == "off":
-                        ballMotor.off()
-                    elif arguments[0] == "out":
-                        ballMotor.on_for_rotations(SpeedPercent(100), 10)
-                        ballMotor.on(SpeedPercent(-100))
-
-
-                    elif arguments[0] == "b":
-                        tank_drive.on_for_rotations(SpeedPercent(-50), SpeedPercent(-50), 1, False, False)
-                    elif arguments[0] == "softright":
-                        tank_drive.on_for_rotations(SpeedPercent(50), SpeedPercent(100), 10)
-                    elif arguments[0] == "softleft":
-                        tank_drive.on_for_rotations(SpeedPercent(100), SpeedPercent(50), 10)
-
-                    else:
-                        sound = Sound()
-                        sound.speak('Jarvis, jerk it a little')
-                        tank_drive.on_for_rotations(SpeedPercent(100), SpeedPercent(100), 0.1)
-                        tank_drive.on_for_rotations(SpeedPercent(-100), SpeedPercent(-100), 0.1)
-                        tank_drive.on_for_rotations(SpeedPercent(100), SpeedPercent(100), 0.1)
-                        tank_drive.on_for_rotations(SpeedPercent(-100), SpeedPercent(-100), 0.1)
-                        tank_drive.on_for_rotations(SpeedPercent(100), SpeedPercent(100), 0.1)
-                        tank_drive.on_for_rotations(SpeedPercent(-100), SpeedPercent(-100), 0.1)
-                        tank_drive.on_for_rotations(SpeedPercent(100), SpeedPercent(100), 0.1)
-                        tank_drive.on_for_rotations(SpeedPercent(-100), SpeedPercent(-100), 0.1)
-                        tank_drive.on_for_rotations(SpeedPercent(100), SpeedPercent(100), 0.1)
-                        tank_drive.on_for_rotations(SpeedPercent(-100), SpeedPercent(-100), 0.1)
-                        tank_drive.on_for_rotations(SpeedPercent(100), SpeedPercent(100), 0.1)
-                        tank_drive.on_for_rotations(SpeedPercent(-100), SpeedPercent(-100), 0.1)
-                        """
 
     finally:
         srv.close()
