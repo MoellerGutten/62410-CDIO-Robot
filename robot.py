cf9a2834181d75451232697c15bfb256f875b20b
MoellerGutten <tobias-alleroed@hotmail.com>
2026-04-15 10:11:19 +0200

    small fix

--- robot.py
+++ robot.py
@@ -67,7 +67,7 @@ def main():
                     elif cmd == CommandName.BACKWARD and args.speed and (args.rotations or args.position or args.seconds):
                         backward(args.speed, args.rotations, args.position, args.seconds, args.brake, args.block)
                     elif cmd == CommandName.TANK_LEFT:
-                        turn(-args.lspeed, -args.rspeed, args.rotations, args.position, args.seconds, args.target_angle, args.brake, args.block)
+                        turn(args.speed, -args.lspeed, -args.rspeed, args.rotations, args.position, args.seconds, args.target_angle, args.brake, args.block)
                     elif cmd == CommandName.TANK_RIGHT:
                         turn(args.speed, args.lspeed, args.rspeed, args.rotations, args.position, args.seconds, args.target_angle, args.brake, args.block)
                     elif cmd == CommandName.BALL_IN:
