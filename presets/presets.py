287b7f709655c5665c28e26da23dd68e77e15bec
MoellerGutten <tobias-alleroed@hotmail.com>
2026-06-10 10:15:40 +0200

    fix: removed unsupported dataclasses

--- presets/presets.py
+++ presets/presets.py
@@ -1,15 +1,11 @@
-from dataclasses import dataclass
-
-@dataclass
-class RobotPreset:
-    reverse_motor = False
-    speed_modifier = 1
-    reverse_direction = False
-
-    # Protect against negative speed modifier values
-    def __post_init__(self):
-        if self.speed_modifier <= 0:
+class RobotPreset(object):
+    def __init__(self, reverse_motor=False, speed_modifier=1, reverse_direction=False):
+        if speed_modifier <= 0:
             raise ValueError("speed_modifier must be positive")
+
+        self.reverse_motor = reverse_motor
+        self.speed_modifier = speed_modifier
+        self.reverse_direction = reverse_direction
 
 
 PRESET_A = RobotPreset(reverse_motor=True, speed_modifier=1, reverse_direction=False)
